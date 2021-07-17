7202b9d37187924c15611f8aa88fc9cabe40646b
steel-hyuk <59719552+steel-hyuk@users.noreply.github.com>
2021-07-17 09:46:59 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -17,5 +17,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from GIT!");
 		System.out.println("Change after First commit!");
+		System.out.println("Change after Second commit!");
 	}
 }
